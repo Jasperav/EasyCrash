39dd4d76c8183d40323073d81e4ad201543c7a42
Jasper <Jasperav@hotmail.com>
2019-06-30 22:55:06 +0200

    commit

---
+++ EasyCrash/ViewController.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidAppear(_ animated: Bool) {
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { (_) in
+            let someInitializable: Initializable.Type = SomeViewController.self
+            
+            self.present(someInitializable.init(), animated: true, completion: nil)
+        }
+    }
+
+}
+
+protocol Initializable: UIViewController {
+    init()
+}
+
+protocol UIViewControllerInitializable: Initializable, UIViewController {}
+
+class BaseTableViewController: UITableViewController {
+    let a: Int
+    
+    init(a: Int) {
+        self.a = a
+        
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+final class SomeViewController: BaseTableViewController, UIViewControllerInitializable {
+    init() {
+        print("init")
+        
+        super.init(a: 10)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
